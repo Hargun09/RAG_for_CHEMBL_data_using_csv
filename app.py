95354d4121f0f8e57b49c89dc248395bff21345c
Hargun09 <128298941+Hargun09@users.noreply.github.com>
2025-07-11 00:43:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import os
 import zipfile
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFaceHub  # <-- not from langchain.llms
 from langchain.vectorstores import FAISS
-from langchain.llms import HuggingFaceHub
+#from langchain.llms import HuggingFaceHub
 from langchain.chains import RetrievalQA
 from huggingface_hub import login
 
@@ -58,10 +59,13 @@ except Exception as e:
     print("Login error:", e)
 
 # ================== LLM ==================
+
 llm = HuggingFaceHub(
     repo_id="google/flan-t5-large",
     model_kwargs={"temperature": 0.5, "max_length": 512}
 )
+
+
 
 # ================== RETRIEVAL CHAIN ==================
 qa_chain = RetrievalQA.from_chain_type(
